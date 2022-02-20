76a0f370c7783b916dbee58fedf775cbd984465b
Lauren <elsee388@protonmail.com>
2022-02-19 20:18:49 -0500

    pushing untracked files

--- 04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ 04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -3,13 +3,12 @@ package com.techelevator;
 public class Tutorial {
 
     public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for(int ix = 0; ix < numbers.length; ix++) {
+            numbers[ix] = ix * 2;
+        }
 
-        // write your code here
-
-
-
-
-
-
+        int result = numbers[3];
+        System.out.println(result);
     }
 }
