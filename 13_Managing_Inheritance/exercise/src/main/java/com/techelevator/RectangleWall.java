fd1ac5c3852c3060f9015c3591855078d9f687cd
Lauren <elsee388@protonmail.com>
2022-03-26 10:42:16 -0400

    All tests passing, calculator working

---
+++ 13_Managing_Inheritance/exercise/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,39 @@
+package com.techelevator;
+
+public class RectangleWall extends Wall{
+    private int length;
+    private int height;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+    public RectangleWall(String name, String color, int length, int height) {
+        super(name, color);
+        this.length = length;
+        this.height = height;
+    }
+
+    @Override
+    public int getArea() {
+        return length * height;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " (" + length + "x" + height + ") rectangle";
+    }
+}
